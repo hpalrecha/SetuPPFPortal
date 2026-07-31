ba510ebdadb846d6ee5045c2d10ee8ba9682e8ea
hpalrecha <hpalrecha@gmail.com>
2026-07-31 15:22:28 +0530

    Update EditServiceModal.tsx

--- client/src/components/modals/EditServiceModal.tsx
+++ client/src/components/modals/EditServiceModal.tsx
@@ -302,7 +302,7 @@ export function EditServiceModal({ open, onOpenChange, service, onSuccess }: Edi
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-2xl">
+      <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Edit Service</DialogTitle>
         </DialogHeader>
